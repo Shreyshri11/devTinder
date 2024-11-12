e80d1a4a815a9e43e37f5eba8b62471fe4cfc6fc
Shreyshri11 <shreysrivastava175@gmail.com>
2024-11-12 17:56:48 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,36 +1,47 @@
-const express = require('express') ;
+const express = require('express');
 //create a new application of express
 
-const app = express() ;
+const app = express();
 // this is an instance of express JS application
 // you had a new web server
 
 //request handler
 
-app.use("/user", (req, res) => {
-    res.send("hahahahahhahaaha") ;
-})
-
 //this will only handle GET call /user
-app.get("/user", (req, res) => {
-    res.send({firstName: "Shrey", lastName: "Srivastava", email: "srivastavashrey01@gmail.com"}) ;
-})
-
-app.post("/user", (req, res) => {
-    // console.log("save data to the database") ;
-    //save data to DB
-    res.send("Data successfully saved to the database") ;
-});
-
-app.delete("/user", (req, res) => {
-    res.send("data deleted successfully") ;
-})
-
-//this will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-    res.send("Hello from the server!") ;
-})
+app.get(
+    "/user",
+    [
+    (req, res, next) => {
+        //route handler
+        console.log("Handling the route user!!");
+        next();
+    },
+    (req, res, next) => {
+        //route handler
+        console.log("Handling the route user 2!!");
+        // res.send("2nd Response!!");
+        next() ;
+    },
+    (req, res, next) => {
+        //route handler
+        console.log("Handling the route user 3!!");
+        // res.send("3rd Response!!");
+        next() ;
+    },
+    (req, res, next) => {
+        //route handler
+        console.log("Handling the route user 4!!");
+        // res.send("4th Response!!");
+        next() ;
+    },
+    (req, res, next) => {
+        //route handler
+        console.log("Handling the route user 5!!");
+        res.send("5th Response!!");
+        next() ;
+    }
+]);
 
 app.listen(3001, () => {
-    console.log("Server is successfully listening on port 3001....") ;
-}) ;
+    console.log("Server is successfully listening on port 3001....");
+});
